54b89c066ac93ac66285fcb736f4d99304b3b2e5
Camilo <castroh24@gmail.com>
2022-02-10 01:00:15 -0600

    productoFormAgregado

--- WpfApp1/views/RegistroProducto.xaml.cs
+++ WpfApp1/views/RegistroProducto.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfApp1.views;
 
-namespace WpfApp1
+namespace WpfApp1.views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RegistroProducto.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistroProducto : Window
     {
-        public MainWindow()
+        public RegistroProducto()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            RegistroProducto registro = new RegistroProducto();
         }
     }
 }
